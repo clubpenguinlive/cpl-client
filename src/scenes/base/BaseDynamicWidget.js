48ef2ecf19435d5c2701c1d80984b9ad5272d796
wizguin <16276645+wizguin@users.noreply.github.com>
2024-11-08 16:00:22 +0000

    Update BaseDynamicWidget once destroy

--- src/scenes/base/BaseDynamicWidget.js
+++ src/scenes/base/BaseDynamicWidget.js
@@ -6,9 +6,7 @@ export default class BaseDynamicWidget extends BaseWidget {
     close() {
         super.close()
 
-        const ui = this.interface
-
-        this.once('destroy', () => ui.removeWidget(this))
+        this.once('destroy', () => this.interface.removeWidget(this))
 
         this.destroy()
     }
