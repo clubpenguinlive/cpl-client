dfebae619d24faa278f00e2a07c0685dc70a132d
wizguin <16276645+wizguin@users.noreply.github.com>
2024-11-16 19:01:00 +0000

    Update BaseScene to use fixed references instead of getters

--- src/scenes/base/BaseScene.js
+++ src/scenes/base/BaseScene.js
@@ -1,35 +1,16 @@
 export default class BaseScene extends Phaser.Scene {
 
     init() {
+        this.crumbs = this.game.crumbs
+        this.network = this.game.network
+        this.soundManager = this.game.soundManager
+
+        this.interface = this.scene.get('InterfaceController')
+        this.memory = this.scene.get('MemoryManager')
+        this.ruffle = this.scene.get('RuffleController')
+        this.world = this.scene.get('WorldController')
+
         this.input.on('pointerover', () => this.interface.resetCursor(this))
-    }
-
-    get crumbs() {
-        return this.game.crumbs
-    }
-
-    get network() {
-        return this.game.network
-    }
-
-    get soundManager() {
-        return this.game.soundManager
-    }
-
-    get memory() {
-        return this.scene.get('MemoryManager')
-    }
-
-    get interface() {
-        return this.scene.get('InterfaceController')
-    }
-
-    get world() {
-        return this.scene.get('WorldController')
-    }
-
-    get ruffle() {
-        return this.scene.get('RuffleController')
     }
 
     getString(...args) {
