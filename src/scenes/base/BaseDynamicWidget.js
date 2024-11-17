cb821cfe48ed5e8c1aa2c72cead941ee0c3da705
wizguin <16276645+wizguin@users.noreply.github.com>
2024-11-17 11:09:41 +0000

    Make sure dynamic widget tweens are all destroyed on close

--- src/scenes/base/BaseDynamicWidget.js
+++ src/scenes/base/BaseDynamicWidget.js
@@ -6,9 +6,23 @@ export default class BaseDynamicWidget extends BaseWidget {
     close() {
         super.close()
 
-        this.once('destroy', () => this.interface.removeWidget(this))
+        this.destroyTweens(this)
 
+        this.once('destroy', () => this.interface.removeWidget(this))
         this.destroy()
+    }
+
+    // Recursively destroy tweens
+    destroyTweens(container) {
+        this.scene.tweens.killTweensOf(container)
+
+        container.each(child => {
+            this.scene.tweens.killTweensOf(child)
+
+            if (child.type === 'Container') {
+                this.destroyTweens(child)
+            }
+        })
     }
 
 }
