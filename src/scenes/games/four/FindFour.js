3c82631f49b5464891d3307d6f0cf0f9768eff4d
wizguin <16276645+wizguin@users.noreply.github.com>
2024-11-17 16:20:58 +0000

    Update FindFour for new system

--- src/scenes/games/four/FindFour.js
+++ src/scenes/games/four/FindFour.js
@@ -1,10 +1,3 @@
-import BaseContainer from '@scenes/base/BaseContainer'
-
-import { Button, DraggableContainer, SimpleButton } from '@components/components'
-
-import FindFourPlayer from './FindFourPlayer'
-
-
 export const preload = {
     key: 'four-pack',
     url: 'assets/media/games/four/four-pack.json',
@@ -13,7 +6,17 @@ export const preload = {
 
 /* START OF COMPILED CODE */
 
-export default class FindFour extends BaseContainer {
+import BaseDynamicWidget from "../../base/BaseDynamicWidget";
+import DraggableContainer from "../../components/DraggableContainer";
+import FindFourPlayer from "./FindFourPlayer";
+import Button from "../../components/Button";
+/* START-USER-IMPORTS */
+
+import SimpleButton from '@scenes/components/SimpleButton'
+
+/* END-USER-IMPORTS */
+
+export default class FindFour extends BaseDynamicWidget {
 
     constructor(scene, x, y) {
         super(scene, x ?? 730, y ?? 340);
@@ -119,11 +122,21 @@ export default class FindFour extends BaseContainer {
 
         /* START-USER-CTR-CODE */
 
-        this.scene = scene
-
         this.counters = []
         this.buttons = []
         this.createButtons()
+
+        this.map = null
+        this.myTurn = null
+        this.currentTurn = 1
+        this.started = false
+
+        this.addListeners()
+
+        scene.events.once('update', () => {
+            this.updateButtons()
+            this.network.send('get_game')
+        })
 
         /* END-USER-CTR-CODE */
     }
@@ -151,21 +164,6 @@ export default class FindFour extends BaseContainer {
         this.network.events.off('start_game', this.handleStartGame, this)
         this.network.events.off('send_move', this.handleSendMove, this)
         this.network.events.off('close_game', this.handleCloseGame, this)
-    }
-
-    show() {
-        this.map = null
-        this.myTurn = null
-        this.currentTurn = 1
-        this.started = false
-
-        this.visible = true
-
-        this.updateButtons()
-        this.updateHover()
-
-        this.addListeners()
-        this.network.send('get_game')
     }
 
     close() {
@@ -364,20 +362,10 @@ export default class FindFour extends BaseContainer {
 
     leaveTable() {
         this.removeListeners()
-        this.resetGame()
-
-        this.visible = false
 
         this.world.client.sendLeaveSeat()
-    }
 
-    resetGame() {
-        for (let counter of this.counters) {
-            counter.destroy()
-        }
-
-        this.player1.reset()
-        this.player2.reset()
+        super.close()
     }
 
     /* END-USER-CODE */
