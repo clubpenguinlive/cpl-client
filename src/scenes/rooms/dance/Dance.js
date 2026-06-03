5f2f4c784144d95965b534515ffc6cc924dc5b47
Nicolas Sanchez <98576999+nickk02@users.noreply.github.com>
2026-06-03 09:44:29 +0000

    Wire Mine room (Mine Shack) and 2nd Boiler entrance (Night Club) to archive room SWFs

--- src/scenes/rooms/dance/Dance.js
+++ src/scenes/rooms/dance/Dance.js
@@ -42,7 +42,7 @@ export default class Dance extends RoomScene {
 
         this.roomTriggers = {
             'town': () => this.triggerRoom(100, 680, 520),
-            'boiler': null,
+            'boiler': () => this.triggerFlashRoom('assets/media/flash/rooms/boiler11.swf'),
             'lounge': () => this.triggerRoom(121, 1200, 760),
             'mix': null
         }
